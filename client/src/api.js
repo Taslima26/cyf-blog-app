2c626dfc13642cf83939089822df69c3143a6b7a
Taslima <61126921+Taslima26@users.noreply.github.com>
2020-12-10 21:33:08 +0000

    changed apis to local now

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://cyf-blog-app.herokuapp.com/api",
+  baseURL: 'http://localhost:3100/api',
 });
 export default client;
 
